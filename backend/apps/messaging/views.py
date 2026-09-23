509e9351e2a2f0a842c2ebb4f1b2236599a8a0f1
Аскар <skrrsnv@gmail.com>
2026-09-23 16:19:29 +0600

    feat: add messages permissions and views were updated

--- backend/apps/messaging/views.py
+++ backend/apps/messaging/views.py
@@ -2,12 +2,13 @@ from rest_framework import generics
 from .models import Message
 from .serializers import MessageSerializer
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsMessageSender
+from apps.conversations.permissions import IsConversationMember
 
 
 class MessageListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
-    
+    permission_classes = [IsAuthenticated, IsConversationMember,]
     def get_queryset(self):
         return Message.objects.filter(
             conversation_id=self.kwargs["conversation_id"]
@@ -18,13 +19,28 @@ class MessageListCreateAPIView(generics.ListCreateAPIView):
             sender=self.request.user,
             conversation_id=self.kwargs["conversation_id"],
         )
-    
 
-class MessageDetailAPIView(generics.RetrieveUpdateAPIView):
+
+class MessageDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
-    
+
     def get_queryset(self):
         return Message.objects.filter(
             conversation_id=self.kwargs["conversation_id"]
         )
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [
+                IsAuthenticated(),
+                IsConversationMember(),
+            ]
+
+        if self.request.method in ["PUT", "PATCH", "DELETE"]:
+            return [
+                IsAuthenticated(),
+                IsConversationMember(),
+                IsMessageSender(),
+            ]
+
+        return [IsAuthenticated()]
